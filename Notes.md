d4cc1de5e6af37a8d751c91452f7721332b2ae39
unknown <francisco-dg-1@hotmail.com>
2023-06-01 04:21:00 -0500

    first commit

---
+++ Notes.md
@@ -0,0 +1,2 @@
+to run aplication :
+    python .\index.py
---
+++ r.bat
@@ -0,0 +1,4 @@
+:: Get into the pipenv and run a local host (http://localhost:5000/)
+:: You must to put the letter "r".
+pipenv shell
+python .\src\index.py
